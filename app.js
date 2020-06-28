e6a6d3d6c0d7a1add18aa4cd1f376ec4025ca726
Samuel Yoo <sam.hc.yoo@gmail.com>
2020-06-28 12:46:03 -0400

    update with app.js

--- app.js
+++ app.js
@@ -10,26 +10,91 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
-
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
+let id = 1;
 
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
+async function main(){
+    const team = [];
 
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
+    const managerData = await inquirer.prompt([
+        {
+            name: "name",
+            type: "input",
+            message: "Enter the manager's name"
+        },
+        {
+            name: "email",
+            type: "input",
+            message: "Enter the email address"
+        },
+        {
+            name: "officeNumber",
+            type: "input",
+            message: "Enter the office number"
+        },
+        {
+            name: "count",
+            type: "input",
+            message: "Enter how many people work under the manager"
+        }
+    ]);
+    team.push(new Manager(managerData.name, id++, managerData.email, managerData.officeNumber, managerData.count));
 
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
+    for(let userCnt = 1; userCnt<=managerData.count; userCnt++){
+        const user = await inquirer.prompt([
+            {
+                name: "type",
+                type: "list",
+                message: `For person ${userCnt}/${managerData.name}`,
+                choices: ["Engineer", "Intern"]
+            }
+        ])
+        if (user.type == "Engineer"){
+            const userData = await inquirer.prompt([
+                {
+                    name: "name",
+                    type: "input",
+                    message: "Enter the enginner's name"
+                },
+                {
+                    name: "email",
+                    type: "input",
+                    message: "Enter the email address"
+                },
+                {
+                    name: "github",
+                    type: "input",
+                    message: "Enter the github user name"
+                }
+            ])
+            team.push(new Engineer(userData.name, id++, userData.email, userData.github));
+        }
+        else{
+            const userData = await inquirer.prompt([
+                {
+                    name: "name",
+                    type: "input",
+                    message: "Enter the intern's name"
+                },
+                {
+                    name: "email",
+                    type: "input",
+                    message: "Enter the email address"
+                },
+                {
+                    name: "school",
+                    type: "input",
+                    message: "Enter the school name"
+                }
+            ])
+            team.push(new Intern(userData.name, id++, userData.email, userData.school));
+        }
+    }
+    const html = render(team);
+
+    fs.writeFileSync (outputPath, html);
+}
+
+main();
